7f10ba8262450a327dd52edb0f27c9fbd75d0941
ikeya_sa <neripapa16@gmail.com>
2023-08-05 21:26:59 +0900

    PHP/Laravel 14 課題7

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profiles;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -12,8 +14,20 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profiles::$rules);
+        
+        $profiles = new Profiles;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profiles->fill($form);
+        $profiles->save();
+
         return redirect('admin/profile/create');
     }
 
