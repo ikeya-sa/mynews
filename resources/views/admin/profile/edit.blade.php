cef409f98daad0da4d1d2129d1e30a089d7be5d6
ikeya_sa <neripapa16@gmail.com>
2023-08-07 16:14:39 +0900

    PHP/Laravel 17 課題2(5)

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -42,6 +42,20 @@
                     @csrf
                     <input type="submit" class="btn btn-primary" value="更新">
                 </form>
+                {{-- 以下を追記 --}}
+                <div class="row mt-5">
+                    <div class="col-md-4 mx-auto">
+                        <h2>編集履歴</h2>
+                        <ul class="list-group">
+                            @if ($profiles_form->profilehistories != NULL)
+                                @foreach ($profiles_form->profilehistories as $profilehistory)
+                                    <li class="list-group-item">{{ $profilehistory->edited_at }}</li>
+                                @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+                {{-- 以上を追記 --}}
             </div>
         </div>
     </div>
