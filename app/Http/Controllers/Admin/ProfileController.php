d265840cf8473f329b0fcbc3c468f36110c62bf4
ikeya_sa <neripapa16@gmail.com>
2023-08-07 15:12:03 +0900

    PHP/Laravel 14 課題7

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Models\Profiles;
+use App\Models\Profile;
 
 class ProfileController extends Controller
 {
@@ -17,9 +17,9 @@ class ProfileController extends Controller
     public function create(Request $request)
     {
         // Validationを行う
-        $this->validate($request, Profiles::$rules);
+        $this->validate($request, Profile::$rules);
         
-        $profiles = new Profiles;
+        $profiles = new Profile;
         $form = $request->all();
         
         // フォームから送信されてきた_tokenを削除する
@@ -34,8 +34,8 @@ class ProfileController extends Controller
 
     public function edit(Request $request)
     {
-        // Profiles Modelからデータを取得する
-        $profiles = Profiles::find($request->id);
+        // Profile Modelからデータを取得する
+        $profiles = Profile::find($request->id);
         if (empty($profiles)) {
             abort(404);
         }
@@ -45,9 +45,9 @@ class ProfileController extends Controller
     public function update(Request $request)
     {
         // Validationをかける
-        $this->validate($request, Profiles::$rules);
+        $this->validate($request, Profile::$rules);
         // News Modelからデータを取得する
-        $profiles = Profiles::find($request->id);
+        $profiles = Profile::find($request->id);
         // 送信されてきたフォームデータを格納する
         $profiles_form = $request->all();
         
